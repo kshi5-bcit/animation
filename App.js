9f16ee55a0d14a009b544f7529ae916dc082a64d
Kevin Shi <kshi5@my.bcit.ca>
2020-02-11 17:25:47 -0800

    added missing semicolon

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import Example from './components/Example'
+import Example from './components/Example';
 
 export default function App() {
   return (
