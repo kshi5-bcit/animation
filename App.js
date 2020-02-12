eeca8740c615d05db29840f78db36d9c026d8947
Kevin Shi <kshi5@my.bcit.ca>
2020-02-11 18:09:06 -0800

    Animatable simple bounce and pulse

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableWithoutFeedback } from 'react-native';
+
+import * as Animatable from 'react-native-animatable';
+import Animated, { Easing } from 'react-native-reanimated';
 
 import Example from './components/Example';
 
 export default function App() {
+    handleViewRef = ref => this.view = ref;
+  
+  bounce = () => this.view.bounce(800).then(endState => console.log(endState.finished ? 'bounce finished' : 'bounce cancelled'));
+  
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <Example />
+      <Animatable.Text animation="pulse" easing="linear" iterationCount={12} style={{ textAlign: 'center'}}>
+        HELLO WORLD!
+      </Animatable.Text>
+        <Animatable.View animation="bounce" duration={800} iterationCount={12}>
+          <Text>Bounce me!</Text>
+        </Animatable.View>
+              <Example />
     </View>
   );
 }
